fd92cfa7c0ca2b7e3785c607124ec7d8ffe2cfb2
Shuai Hao <Shuai Hao>
2016-09-27 09:37:32 -0700

    full version web server

--- userStory/app/routes/api.js
+++ userStory/app/routes/api.js
@@ -1,9 +1,25 @@
-var Users = require('../models/user');
-
+var User = require('../models/user');
+var Story = require('../models/story');
 var config = require('../../config');
 
 var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
+
+function createToken(user){
+
+	var token = jsonwebtoken.sign({
+		_id: user._id,
+		name: user.name,
+		username: user.username
+	}, secretKey, {
+		expiresInMinute: 1440
+	});
+
+
+	return token;
+
+}
 
 module.exports = function(app, express){
 
@@ -13,13 +29,13 @@ module.exports = function(app, express){
 
 	api.post('/signup', function(req, res){
 
-		var users = new Users({
+		var user = new User({
 			name: req.body.name,
 			username: req.body.username,
 			Password: req.body.password
 		});
 
-		users.save(function(err){
+		user.save(function(err){
 			if(err){
 				res.send(err);
 				return;
@@ -32,7 +48,7 @@ module.exports = function(app, express){
 
 	api.get('/users', function(req, res){
 
-		Users.find({}, function(err, users){
+		User.find({}, function(err, users){
 			if(err){
 				res.send(err);
 				return;
@@ -43,6 +59,107 @@ module.exports = function(app, express){
 		});
 	});
 
+// why the login api doesnt work
+	api.post('/login', function(req, res){
+
+		User.findOne({
+			username: req.body.username
+		}).select('password').exec(function(err, user){
+
+			if(err) throw err;
+
+			if(!user) {
+
+				res.send({message: "User doesn't exist"});
+			} else if(user){
+
+				var validPassword = user.comparePassword(req.body.password);
+
+				if(!validPassword){
+					res.send({ message: "Invalid Password"});
+				} else {
+
+					var token = createToken(user);
+
+					res.json({
+						success: true,
+						message: "Successfuly login!",
+						token: token
+					});
+				}
+			}
+		});
+	});
+
+	api.use(function(req,res,next){
+
+
+		console.log("somebody just came to our app!");
+
+		var token = req.body.token || req.query.token||req.headers['x-access-token'];
+
+		// check if token exist
+		if(token) {
+
+			jsonwebtoken.veryfy(token, secretKey, function(err, decoded){
+
+				if(err) {
+					res.status(403).send({success: false, message:"Failed to authenticate user"});
+				
+				}else {
+
+					req.decoded = decoded;
+
+					next();
+				}
+			});
+
+		} else{
+
+			res.status(403).send({success:false, message: "no token provided"});
+			}
+
+	});
+
+
+	// provide a legitimate token
+
+	api.route('/')
+
+		.post(function(req, res){
+
+			var story = new Story({
+
+				creator: req.decoded.id,
+				content: req.body.content,
+
+			});
+
+			story.save(function(err) {
+
+				if(err) {
+					res.send(err);
+					return
+				}
+				res.json({message:"new story is blablabla"});
+			});
+		})
+
+		.get(function(req, res){
+
+			Story.find({creator: req.decoded.id}, function(err, stories){
+
+				if(err){
+					res.send(err);
+					return;
+				}
+				
+				res.json(stories);
+
+			});
+
+
+		});
 
 	return api;
 
